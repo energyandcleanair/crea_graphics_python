d3b200716203af08228e16ad226b46ebb1e72919
rosa <rosa@energyandcleanair.org>
2024-09-10 17:33:02 +0300

    Bug fix

--- src/crea_graphics/colors/__init__.py
+++ src/crea_graphics/colors/__init__.py
@@ -1,4 +1,4 @@
-from crea_colors import primary, primary_cont, primary_cont_r, \
+from .crea_colors import primary, primary_cont, primary_cont_r, \
     primary_blue, primary_blue_cont, primary_blue_cont_r, \
     goodbad, goodbad_cont, goodbad_cont_r, \
     goodbad_red_cont, goodbad_red_cont_r, goodbad_light, goodbad_light_r, \
