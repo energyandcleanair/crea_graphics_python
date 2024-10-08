164bda7d077b68e3f6ce5ee243bd6d966a4163f1
rosa <rosa@energyandcleanair.org>
2024-10-08 15:26:54 +0300

    forgot to add imports of new colormaps to init in last commit, fixed

--- src/crea_graphics/colors/__init__.py
+++ src/crea_graphics/colors/__init__.py
@@ -2,4 +2,5 @@ from .crea_colors import primary, primary_cont, primary_cont_r, \
     primary_blue, primary_blue_cont, primary_blue_cont_r, \
     goodbad, goodbad_cont, goodbad_cont_r, \
     goodbad_red_cont, goodbad_red_cont_r, goodbad_light, goodbad_light_r, \
+    goodbad_lighter, goodbad_lighter_r, goodbad_lightgreen, goodbad_lightgreen_r, \
     secondary, secondary_cont, secondary_cont_r, secondary_noblack, secondary_noblack_r
